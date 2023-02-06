83189c4f5c0ba462f31c6b745805cbbc29ce4e22
RasecMH <rasecmh0@gmail.com>
2023-02-06 10:08:48 -0300

    fix node v

--- app/backend/api/server.js
+++ app/backend/api/server.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
 require("dotenv/config");
 const PORT = process.env.APP_PORT || 3001;
-new index_1.App().start(PORT);
+new index_1.default().start(PORT);
 //# sourceMappingURL=server.js.map
